9dd5a3ccdf295ea9b5f0a0fed844d82e2e666ca3
zirons1 <zirons@udel.edu>
2018-12-07 12:37:06 -0500

    Adding file that has hall effect sensor test
    This hall effect test polls two hall effect sensors for the presence of a magnet.

---
+++ hall_effect_test.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int hall1 = analogRead(0);
+  int hall2 = analogRead(1);
+
+  if (hall1 == 0){
+    Serial.print("AHH! A magnet!");
+  }else{
+    Serial.print("Phew, no magnets.");
+  }
+  Serial.print("\t\t\t");
+  if (hall2 == 0){
+    Serial.println("AHH! A magnet!");
+  }else{
+    Serial.println("Phew, no magnets.");
+  }
+}
