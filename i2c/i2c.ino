5ce18ee3b0dcdf857ddc8501e42072937e164e2d
Collin Reilly Clark <crclark96@gmail.com>
2019-05-12 16:47:34 -0400

    working version of i2c -- IN FOLDER

--- i2c/i2c.ino
+++ i2c/i2c.ino
@@ -4,6 +4,17 @@
 int number = 0;
 int state = 0;
 
+int flag = 0;
+
+int board[] = {1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 1, 
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0, 
+               0, 0, 0, 0, 0, 0, 0, 0,
+               1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 1};
+
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(9600); // start serial for output
@@ -23,27 +34,36 @@ void loop() {
 
 // callback for received data
 void receiveData(int byteCount){
-
+  int counter = 0;
+  int start_index = 0;
+  int finish_index = 0;
   while(Wire.available()) {
     number = Wire.read();
     Serial.print("data received: ");
     Serial.println(number);
 
-    if (number == 1){
-
-      if (state == 0){
-        digitalWrite(13, HIGH); // set the LED on
-        state = 1;
-      }
-      else{
-        digitalWrite(13, LOW); // set the LED off
-        state = 0;
-      }
+    if (counter == 1){
+      start_index = number;
+    }else if (counter == 2){
+      finish_index = number;
     }
+    counter++;
   }
+  Serial.print("start and finish: ");
+  Serial.print(start_index);
+  Serial.print(" ");
+  Serial.println(finish_index);
+
+  //Call motors
+
+  board[start_index] = 0;
+  board[finish_index] = 1;
 }
 
 // callback for sending data
 void sendData(){
-  Wire.write(number);
+  Serial.print("SEND: ");
+  Serial.println(board[flag]);
+  Wire.write(board[flag]);
+  flag = (flag+1)%64;
 }
